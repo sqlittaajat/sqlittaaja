cb35316926d9a6f1bbba436e09eee3869f18ce2a
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-04-02 19:08:59 +0300

    refactor: move argument reading to __init__

--- src/sqlittaaja/__init__.py
+++ src/sqlittaaja/__init__.py
@@ -1,6 +1,41 @@
-from .extractor import readArgs
+from .extractor import extract
+import sys
+import getopt
 
 
 def main():
     print("SQLittaaja")
-    readArgs()
+    read_args()
+
+
+def read_args():
+    """Reads command line arguments."""
+
+    argv = sys.argv[1:]
+    if not argv:
+        print("-z <zipfile> -e <exercise>")
+        sys.exit(2)
+
+    zip = ""
+    exercise = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "hz:e:", ["zipfile=", "exercise="])
+    except getopt.GetoptError:
+        # error
+        print("-z <zipfile> -e <exercise>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            # help
+            print("-z <zipfile> -e <exercise>")
+            sys.exit()
+        elif opt in ("-z", "--zipfile"):
+            # zip name
+            zip = arg
+        elif opt in ("-e", "--exercise"):
+            # exercise name
+            exercise = arg
+
+    files = extract(zip)
+    print(files)
--- src/sqlittaaja/extractor.py
+++ src/sqlittaaja/extractor.py
@@ -1,40 +1,4 @@
-import sys
-import getopt
 import zipfile
-import os
-
-
-def readArgs():
-    """Reads command line arguments."""
-
-    argv = sys.argv[1:]
-    if not argv:
-        print("-z <zipfile> -e <exercise>")
-        sys.exit(2)
-
-    zip = ""
-    exercise = ""
-
-    try:
-        opts, args = getopt.getopt(argv, "hz:e:", ["zipfile=", "exercise="])
-    except getopt.GetoptError:
-        # error
-        print("-z <zipfile> -e <exercise>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-h":
-            # help
-            print("-z <zipfile> -e <exercise>")
-            sys.exit()
-        elif opt in ("-z", "--zipfile"):
-            # zip name
-            zip = arg
-        elif opt in ("-e", "--exercise"):
-            # exercise name
-            exercise = arg
-
-    files = extract(zip)
-    print(files)
 
 
 def extract(file):
@@ -43,5 +7,5 @@ def extract(file):
     try:
         with zipfile.ZipFile(file, "r") as zip_ref:
             return {name: zip_ref.read(name) for name in zip_ref.namelist()}
-    except:
+    except Exception:
         print("Error with unzipping")
