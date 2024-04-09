845d2e2f17f3cadc77858d94bd2a2067233040b1
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-04-09 20:15:46 +0300

    feat: pretty print table

--- src/sqlittaaja/printer.py
+++ src/sqlittaaja/printer.py
@@ -1,6 +1,21 @@
-def print_scores(student_scores):
+def print_scores(student_scores: dict[str, int]):
     """Prints student scores."""
 
-    print("STUDENT SCORES")
+    name_col = "student name"
+    score_col = "score"
+    # Determine the maximum width.
+    max_name_len = max(len(max(student_scores.keys(), key=len)), len(name_col))
+
+    # Separator line.
+    sep = f"+-{'-' * max_name_len}-+-{'-' * len(score_col)}-+"
+
+    # Header part.
+    print(sep)
+    print(f"| {{:^{max_name_len}}} | {{}} |".format(name_col, score_col))
+    print(sep)
+
+    # Each row.
     for student_name, score in student_scores.items():
-        print(f"{student_name}: {score}")
+        fmt = f"| {{:<{max_name_len}}} | {{:>{len(score_col)}}} |"
+        print(fmt.format(student_name, score))
+    print(sep)
