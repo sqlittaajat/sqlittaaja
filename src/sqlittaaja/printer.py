c3d949da6b2961c8ffd4e240cb7e7a98c726489a
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-04-09 20:21:02 +0300

    feat: use box drawing characters

--- src/sqlittaaja/printer.py
+++ src/sqlittaaja/printer.py
@@ -6,16 +6,13 @@ def print_scores(student_scores: dict[str, int]):
     # Determine the maximum width.
     max_name_len = max(len(max(student_scores.keys(), key=len)), len(name_col))
 
-    # Separator line.
-    sep = f"+-{'-' * max_name_len}-+-{'-' * len(score_col)}-+"
-
     # Header part.
-    print(sep)
-    print(f"| {{:^{max_name_len}}} | {{}} |".format(name_col, score_col))
-    print(sep)
+    print(f"┏━{'━' * max_name_len}━┳━{'━' * len(score_col)}━┓")
+    print(f"┃ {{:^{max_name_len}}} ┃ {{}} ┃".format(name_col, score_col))
+    print(f"┡━{'━' * max_name_len}━╇━{'━' * len(score_col)}━┩")
 
     # Each row.
     for student_name, score in student_scores.items():
-        fmt = f"| {{:<{max_name_len}}} | {{:>{len(score_col)}}} |"
+        fmt = f"│ {{:<{max_name_len}}} │ {{:>{len(score_col)}}} │"
         print(fmt.format(student_name, score))
-    print(sep)
+    print(f"└─{'─' * max_name_len}─┴─{'─' * len(score_col)}─┘")
