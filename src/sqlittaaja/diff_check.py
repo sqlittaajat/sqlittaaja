a96ab63e755af5b2d467ca49dd39de52905075dd
Lassi Inkinen <lassi.inkinen@tuni.fi>
2024-04-12 14:22:11 +0300

    docs: add comments

--- src/sqlittaaja/diff_check.py
+++ src/sqlittaaja/diff_check.py
@@ -16,17 +16,20 @@ def compute_similarity(config: dict[str, Any]) -> dict[str, int]:
     """Check students' exercises."""
 
     similarity_matrix = {}
+
     with ZipFile(config["exercise"]["path"], "r") as exercises:
         extracted = extract(exercises)
-
+        # Go through each students file against other files
         for file1, content1 in extracted.items():
-            if file1.endswith('.sql'):
+            if file1.endswith('.sql'): # Only .sql files
                 for file2, content2 in extracted.items():
                     if file2.endswith('.sql') and file1 != file2:  # Exclude self-comparisons
                         similarity_ratio = compare_files(content1, content2)
                         if similarity_ratio >= threshold:
+                            # Make more readable: student name + filename
                             student1 = student_info(file1)[0]+"/"+file1.rsplit('/', 1)[-1]
                             student2 = student_info(file2)[0]+"/"+file2.rsplit('/', 1)[-1]
 
                             similarity_matrix[(student1, student2)] = str(round(similarity_ratio, 2))+"%"
+
         return similarity_matrix
