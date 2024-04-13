083e85b5d8963f603dae0777fdff89b9340bf69a
Lassi Inkinen <lassi.inkinen@tuni.fi>
2024-04-13 14:56:00 +0300

    fix: remove if

--- src/sqlittaaja/diff_check.py
+++ src/sqlittaaja/diff_check.py
@@ -21,24 +21,17 @@ def compute_similarity(extracted):
 
     # Go through each students file against other files
     for file1, content1 in extracted.items():
-        if file1:
-            for file2, content2 in extracted.items():
-                if (
-                    # Exclude self-comparisons
-                    file2 and file1 != file2
-                ):
-                    similarity_ratio = compare_files(content1, content2)
-                    if similarity_ratio >= threshold:
-                        # Make more readable: student name + filename
-                        student1 = (
-                            student_info(file1)[0] + "/" + file1.rsplit("/", 1)[-1]
-                        )
-                        student2 = (
-                            student_info(file2)[0] + "/" + file2.rsplit("/", 1)[-1]
-                        )
+        for file2, content2 in extracted.items():
+            # Exclude self-comparisons
+            if file1 != file2:
+                similarity_ratio = compare_files(content1, content2)
+                if similarity_ratio >= threshold:
+                    # Make more readable: student name + filename
+                    student1 = student_info(file1)[0] + "/" + file1.rsplit("/", 1)[-1]
+                    student2 = student_info(file2)[0] + "/" + file2.rsplit("/", 1)[-1]
 
-                        similarity_matrix[(student1, student2)] = (
-                            str(round(similarity_ratio, 2)) + "%"
-                        )
+                    similarity_matrix[(student1, student2)] = (
+                        str(round(similarity_ratio, 2)) + "%"
+                    )
 
     return similarity_matrix
