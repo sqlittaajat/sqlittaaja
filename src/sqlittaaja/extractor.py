1b86eeb681b02e298ddc423858c2859ef40cb277
Lassi Inkinen <lassi.inkinen@tuni.fi>
2024-04-13 18:32:35 +0300

    fix: remove unused function

--- src/sqlittaaja/extractor.py
+++ src/sqlittaaja/extractor.py
@@ -13,31 +13,6 @@ def extract(path: str) -> dict[str, str]:
             filter(lambda name: not name.endswith(os.path.sep), zip.namelist())
         }
 
-
-def find_init_scripts(contents: dict[str, str]) -> str:
-    """Find SQL initialization scripts."""
-
-    # We want to use two lists here as the creation tables should come before
-    # population tables.
-    create_tables = []
-    populate_tables = []
-
-    for path in contents:
-        name, ext = os.path.splitext(os.path.split(path)[1])
-        # Skip everything that isn't a SQL file.
-        if ext != ".sql":
-            continue
-
-        # Append to the correct table.
-        if name.endswith("create_table"):
-            create_tables.append(path)
-        elif name.endswith("populate_table"):
-            populate_tables.append(path)
-
-    all_tables = create_tables + populate_tables
-    return "\n".join(contents[name] for name in all_tables)
-
-
 def student_info(name: str) -> (str, str):
     """Extract student information from file path."""
 
