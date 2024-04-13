87d6a793342d5e9e08fc2f1bd85920085de56e07
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-04-13 16:44:47 +0300

    feat: separate lines in the table

--- src/sqlittaaja/printer.py
+++ src/sqlittaaja/printer.py
@@ -10,13 +10,15 @@ def print_scores(
             [
                 student_name,
                 str(score),
-                ", ".join(
+                # Show similarity for each student.
+                "\n".join(
                     f"{similarity[0]} ({str(round(similarity[1] * 100, 2))}%)"
                     for similarity in answer_similarities.get(student_name, [])
                 ),
             ]
             for student_name, score in student_scores.items()
-        ]
+        ],
+        separators=True,
     )
 
 
@@ -25,7 +27,7 @@ def print_table(table: list[list[str]], separators: bool = False):
 
     max_column_lens = [
         # Calculate maximum length for each column.
-        max([len(table[j][i]) for j, _ in enumerate(table)])
+        max([len(max(table[j][i].splitlines(), key=len)) for j, _ in enumerate(table)])
         for i, _ in enumerate(table[0])
     ]
 
@@ -42,16 +44,28 @@ def print_table(table: list[list[str]], separators: bool = False):
 
     # Print each row one-by-one.
     for i, row in enumerate(table[1:]):
-        print(
-            "│ "
-            + " │ ".join(
-                [
-                    value.ljust(max_column_lens[j])
-                    for j, value in enumerate(table[i + 1])
-                ]
+        actual_rows = []
+        for j, field in enumerate(row):
+            # Go through each line for each field.
+            for k, line in enumerate(field.splitlines()):
+                if k >= len(actual_rows):
+                    # Add a new empty row if we have more lines.
+                    actual_rows.append([""] * len(row))
+                # Set the value for the field on this line (row) or keep the default.
+                actual_rows[k][j] = line
+
+        # Print each line (row).
+        for actual_row in actual_rows:
+            print(
+                "│ "
+                + " │ ".join(
+                    [
+                        value.ljust(max_column_lens[j])
+                        for j, value in enumerate(actual_row)
+                    ]
+                )
+                + " │"
             )
-            + " │"
-        )
         if separators and i + 1 < len(table) - 1:
             print(
                 "├─" + "─┼─".join(["─" * max_len for max_len in max_column_lens]) + "─┤"
