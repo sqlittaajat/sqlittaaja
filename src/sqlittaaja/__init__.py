f94d30ce5463b33fe82d03790d4d15194787b167
Noel Tulirinta <noel.tulirinta@tuni.fi>
2024-04-29 20:31:34 +0300

    Read html table styles from style.css

--- src/sqlittaaja/__init__.py
+++ src/sqlittaaja/__init__.py
@@ -2,9 +2,10 @@ from sqlittaaja.output import print_scores, html_scores, indent
 from sqlittaaja.config import read_args
 from sqlittaaja.checker import check_exercises, compute_similarity
 from sqlittaaja.extractor import extract
-from sqlittaaja.html_style import report_style
 import tempfile
 import webbrowser
+import sys
+import os
 
 
 def main():
@@ -18,7 +19,7 @@ def main():
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>SQLittaaja Report</title>
-    <style>{report_style}</style>
+    <style>{report_style()}</style>
   </head>
   <body>
 """
@@ -70,3 +71,15 @@ def main():
 
         if config.open_report:
             webbrowser.open(file.name)
+
+
+def report_style() -> str:
+    """Reads style.css file and returns content."""
+    try:
+        base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+        style_css_path = os.path.join(base_path, "style.css")
+        print(style_css_path)
+        with open(style_css_path, encoding="utf-8") as style_file:
+            return style_file.read()
+    except FileNotFoundError:
+        return ""
