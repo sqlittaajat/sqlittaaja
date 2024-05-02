7394a2af59df9b1e91669bad34a6bdde189c6cc3
Lassi Inkinen <lassi.inkinen@tuni.fi>
2024-05-02 23:31:56 +0300

    refactor: code style

--- src/sqlittaaja/diff_html.py
+++ src/sqlittaaja/diff_html.py
@@ -2,7 +2,10 @@ import difflib
 
 make_table = difflib.HtmlDiff(wrapcolumn=50).make_table
 
-def generate_html_table(similarity_results: dict[tuple[str, str], float], extracted: dict[str, str]) -> str:
+
+def generate_html_table(
+    similarity_results: dict[tuple[str, str], float], extracted: dict[str, str]
+) -> str:
     """Generate HTML table from flagged exercises."""
 
     tables = ""
@@ -11,11 +14,16 @@ def generate_html_table(similarity_results: dict[tuple[str, str], float], extrac
         for student2_key, similarity in flags:
             student2_name = student2_key.split("_")[0]
             # Find the key in extracted_data by matching the first part
-            content1 = extracted[[key for key in extracted.keys() if key.startswith(student1_name)][0]]
-            content2 = extracted[[key for key in extracted.keys() if key.startswith(student2_name)][0]]
-            tables += make_table(content1.splitlines(),
-                                 content2.splitlines(),
-                                 fromdesc=f"{student1_name}: {str(round(similarity * 100, 2))}%",
-                                 todesc=f"{student2_name}: {str(round(similarity * 100, 2))}%")
+            content1 = extracted[
+                [key for key in extracted.keys() if key.startswith(student1_name)][0]
+            ]
+            content2 = extracted[
+                [key for key in extracted.keys() if key.startswith(student2_name)][0]
+            ]
+            tables += make_table(
+                content1.splitlines(),
+                content2.splitlines(),
+                fromdesc=f"{student1_name}: {str(round(similarity * 100, 2))}%",
+                todesc=f"{student2_name}: {str(round(similarity * 100, 2))}%",
+            )
     return tables
-
